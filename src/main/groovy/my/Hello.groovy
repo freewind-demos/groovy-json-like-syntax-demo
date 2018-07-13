db79f2c4d68c1d51ba92c9f664df4b88b48c8f57
freewind <nowindlee@gmail.com>
2018-07-14 00:51:30 +0800

    demo

--- src/main/groovy/my/Hello.groovy
+++ src/main/groovy/my/Hello.groovy
@@ -1,7 +1,25 @@
 package my
 
-static def hello(String name) {
-    return "Hello, $name!"
+class User {
+    String name
+    String words
+
+    def play(String sport) {
+        println("I like playing $sport")
+    }
 }
 
-println(hello("Groovy"))
+def hello(Closure config) {
+    def user = new User()
+    config.resolveStrategy = Closure.DELEGATE_ONLY
+    config.delegate = user
+    config.call()
+    println(user.name)
+    println(user.words)
+}
+
+hello {
+    setName "Groovy"
+    words = "you are groovy"
+    play "Football"
+}
